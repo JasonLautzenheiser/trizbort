549d2bcd9e36ebdcde60c5ac2e89944caae48d42
andrewschultz <schultz.andrew@sbcglobal.net>
2015-04-26 09:56:15 -0500

    batch file touchup

--- testing/basic-text.bat
+++ testing/basic-text.bat
@@ -1,26 +1,30 @@
 echo Version testing here. All the files should look the same except if I explicitly mention new features are added.
 echo The files aren't perfectly identical. Their trizbort versions are different.
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\basics-1570.trizbort
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\basics-1571.trizbort
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\basics-1580.trizbort
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\basics-1581.trizbort
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\basics-1582.trizbort
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\basics-1584.trizbort
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\basics-1585.trizbort
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\basics-1586.trizbort
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\basics-1587.trizbort
+
+::You should be able to run this batch file from Trizbort with no problem, but if not, you can change the variable below.
+set TESTBASE=.
+
+%TESTBASE%\basics-1570.trizbort
+%TESTBASE%\basics-1571.trizbort
+%TESTBASE%\basics-1580.trizbort
+%TESTBASE%\basics-1581.trizbort
+%TESTBASE%\basics-1582.trizbort
+%TESTBASE%\basics-1584.trizbort
+%TESTBASE%\basics-1585.trizbort
+%TESTBASE%\basics-1586.trizbort
+%TESTBASE%\basics-1587.trizbort
 echo All 6 border types appear below.
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\borders.trizbort
+%TESTBASE%\borders.trizbort
 echo Connection types and combinations are tested here.
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\connections.trizbort
+%TESTBASE%\connections.trizbort
 echo All 9 room type fills are tested here. The center is blank. They should form a white octagon.
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\fills.trizbort
+%TESTBASE%\fills.trizbort
 echo All 9 object placements are tested here. The object is listed in the room in the center.
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\fills.trizbort
+%TESTBASE%\fills.trizbort
 echo Stalk lengths are tested: 16, 32, 48
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\stalk-length-16.trizbort
+%TESTBASE%\stalk-length-16.trizbort
 echo Stalk lengths are tested: 16, 32, 48
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\stalk-length-32.trizbort
+%TESTBASE%\stalk-length-32.trizbort
 echo Stalk lengths are tested: 16, 32, 48
-c:\Users\Andrew\Documents\GitHub\trizbort\testing\stalk-length-48.trizbort
+%TESTBASE%\stalk-length-48.trizbort
 

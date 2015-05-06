bdfbc2df6b36933fc55986294a0f2fc571c62e65
andrewschultz <schultz.andrew@sbcglobal.net>
2015-05-05 22:26:36 -0500

    added 1588 test file and ascii-char stress case

--- testing/testsync.pl
+++ testing/testsync.pl
@@ -20,10 +20,14 @@ for $thistriz (@triz)
 {
   if ($thistriz =~ /\.trizbort/i)
   {
-    if (!$trizfile{$thistriz}) { print "Orphaned trizbort file: \%TESTBASE\%\\$thistriz\n"; }
+    if (!$trizfile{$thistriz}) { print "Orphaned trizbort file: \%TESTBASE\%\\$thistriz\n"; $orphanedFiles++; }
   }
 }
 
+if ((!$orphanedFiles) && (!$badFiles)) { print "Hooray! No orphaned *.trizbort files, and no bad references in .bat files!\n"; }
+elsif ($orphanedFiles == 0) { print "There are no orphaned files in the test directory, but there are $badFiles bad files in various batch files.\n"; }
+elsif ($badFiles == 0) { print "There are no bad files, but there are $orphanedFiles orphaned files in the test directory.\n"; }
+else { print "$orphanedFiles orphaned files in the test directory, and $badFiles bad files in the batch files.\n"; } 
 
 sub getTriz
 {
@@ -36,7 +40,7 @@ sub getTriz
     {
       chomp($a);
       $a =~ s/.*\\//g;
-      if (! -f "$a") { print "$a in $_[0] is not a valid file.\n"; }
+      if (! -f "$a") { print "$a in $_[0] is not a valid file.\n"; $badFiles++; }
       $trizfile{$a} = $_[0];
     }
   }

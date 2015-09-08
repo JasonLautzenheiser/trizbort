d4033e83c9785f9b819b1891201903a19b6c7765
andrewschultz <schultz.andrew@sbcglobal.net>
2015-09-08 00:52:51 -0500

    Fix for #134

--- Settings.cs
+++ Settings.cs
@@ -948,6 +948,8 @@ namespace Trizbort
       ConnectionStalkLength = element["rooms"]["connectionStalkLength"].ToFloat(s_connectionStalkLength);
       PreferredDistanceBetweenRooms = element["rooms"]["preferredDistanceBetweenRooms"].ToFloat(s_connectionStalkLength*2); // introduced in v1.2, hence default based on existing setting
       DefaultRoomName = element["rooms"]["defaultRoomName"].Text;
+      if (string.IsNullOrEmpty(DefaultRoomName)) //Fix for Bug#132: Trizbort needs a non-empty default
+          DefaultRoomName = "Cave";
 
       HandleSize = element["ui"]["handleSize"].ToFloat(s_handleSize);
       SnapToElementSize = element["ui"]["snapToElementSize"].ToFloat(s_snapToElementSize);

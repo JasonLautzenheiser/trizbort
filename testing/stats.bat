4b2db3ac57b0afa1a0383c57f1768c75b345d0ff
andrewschultz <schultz.andrew@sbcglobal.net>
2015-09-12 10:48:24 -0500

    added some test cases for dead end rooms

--- testing/stats.bat
+++ testing/stats.bat
@@ -8,3 +8,7 @@ echo Counting a survey of general options. Use this after adding any stat featur
 %TESTBASE%\map-stats-general.trizbort
 echo Counting dead ends.
 %TESTBASE%\map-stats-dead-ends.trizbort
+echo Checking dead end start room.
+%TESTBASE%\map-stats-dead-end-start-room.trizbort
+echo Checking dead end only room.
+%TESTBASE%\map-stats-dead-end-only-room.trizbort

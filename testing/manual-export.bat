0f58510f5872c55a014410762dfe675cb1295a55
andrewschultz <schultz.andrew@sbcglobal.net>
2015-12-22 19:55:10 -0600

    extended char testfile + testsync tweak

---
+++ testing/manual-export.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set TESTBASE=.
+
+echo This file contains test cases for exporting code from Trizbort.
+echo Instructions should be contained in a marquee room at the top.
+
+rem skip warning for reused test file
+echo Manual test case. Export to all formats via the clipboard and view in a text editor. Make sire the accented e's become e's. (issue #234)
+%TESTBASE%\ext-chars-test.trizbort

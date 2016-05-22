161464bb5cd9653a92d61f44c3f7249e552e40d0
Jason Lautzenheiser <lautzenheiser.jason@gmail.com>
2016-05-21 20:43:14 -0400

    refactoring

---
+++ Domain/Controllers/CanvasController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Trizbort.Domain.Commands;
+
+namespace Trizbort.Domain.Controllers
+{
+  public class CanvasController
+  {
+    private readonly Canvas canvas;
+
+    public CanvasController(Canvas canvas)
+    {
+      this.canvas = canvas;
+    }
+
+    public void SelectRoomClosestToCenterOfViewport()
+    {
+      // select the room closest to the center of the viewport
+      var viewportCenter = canvas.Viewport.Center;
+      Room closestRoom = null;
+      var closestDistance = float.MaxValue;
+      foreach (var element in Project.Current.Elements.OfType<Room>())
+      {
+        var roomCenter = element.InnerBounds.Center;
+        var distance = roomCenter.Distance(viewportCenter);
+
+        if (!(distance < closestDistance)) continue;
+        closestRoom = element;
+        closestDistance = distance;
+      }
+
+      canvas.SelectedElement = closestRoom;
+      EnsureVisible(canvas.SelectedElement);
+    }
+
+    public void SelectStartRoom()
+    {
+      var startRoom = Project.Current.Elements.OfType<Room>().FirstOrDefault(p => p.IsStartRoom);
+      if (startRoom != null)
+      {
+        canvas.SelectedElement = startRoom;
+        EnsureVisible(startRoom);
+      }
+
+    }
+
+    public void EnsureVisible(Element element)
+    {
+      if (element == null) return;
+      var rect = Rect.Empty;
+      rect = element.UnionBoundsWith(rect, false);
+      if (rect != Rect.Empty)
+      {
+        canvas.Origin = rect.Center;
+      }
+    }
+
+    public void SetConnectionFlow(ConnectionFlow connectionFlow)
+    {
+      var elements = canvas.SelectedConnections;
+      setConnectionFlow(elements, connectionFlow);
+    }
+
+    private void setConnectionFlow(List<Connection> connections, ConnectionFlow connectionFlow)
+    {
+      connections.ForEach(p=>p.Flow = connectionFlow);
+      canvas.NewConnectionFlow = connectionFlow;
+    }
+
+    public void SetConnectionStyle(ConnectionStyle style)
+    {
+      var elements = canvas.SelectedConnections;
+      setConnectionStyle(elements, style);
+    }
+
+    private void setConnectionStyle(List<Connection> connections, ConnectionStyle style)
+    {
+      connections.ForEach(p=>p.Style = style);
+      canvas.NewConnectionStyle = style;
+    }
+
+    public void SetConnectionLabel(ConnectionLabel label)
+    {
+      var elements = canvas.SelectedConnections;
+      setConnectionLabel(elements,label);
+    }
+
+    private void setConnectionLabel(List<Connection> connections, ConnectionLabel label)
+    {
+      connections.ForEach(p => p.SetText(label));
+      canvas.NewConnectionLabel = label;
+    }
+
+  }
+}
---
+++ Domain/Controllers/CommandController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+using Trizbort.Domain.Commands;
+using Trizbort.Domain.Enums;
+
+namespace Trizbort.Domain.Controllers
+{
+  public class CommandController
+  {
+    private readonly Canvas canvas;
+
+    public CommandController(Canvas canvas)
+    {
+      this.canvas = canvas;
+    }
+
+    public void SelectRegions()
+    {
+      var regions = canvas.SelectedRooms.Select(p => p.Region).Distinct().ToList();
+      var cmd = new SelectCommand();
+      cmd.Execute(canvas, SelectTypes.Region, regions);
+    }
+
+    public void Select(SelectTypes type)
+    {
+      var cmd = new SelectCommand();
+      cmd.Execute(canvas, type);
+    }
+
+    public void SelectStartRoom()
+    {
+      var controller = new CanvasController(canvas);
+      controller.SelectStartRoom();
+    }
+
+    public void SelectRoomClosestToCenterOfViewport()
+    {
+      var controller = new CanvasController(canvas);
+      controller.SelectRoomClosestToCenterOfViewport();
+    }
+
+    public void SetRoomShape(RoomShape shape)
+    {
+      RoomController controller = new RoomController();
+      controller.SetRoomShape(canvas.SelectedRooms,shape);
+    }
+
+    public void SetRoomLighting(LightingActionType type)
+    {
+      RoomController controller = new RoomController();
+      switch (type)
+      {
+        case LightingActionType.Toggle:
+          controller.ToggleDarkness(canvas.SelectedRooms);
+          break;
+        case LightingActionType.ForceLight:
+          controller.ForceLighted(canvas.SelectedRooms);
+          break;
+        case LightingActionType.ForceDark:
+          controller.ForceDarkness(canvas.SelectedRooms);
+          break;
+        default:
+          throw new ArgumentOutOfRangeException(nameof(type), type, null);
+      }
+    }
+
+    public void ShowElementProperties(Element element)
+    {
+      var controller = new ElementController();
+      controller.ShowElementProperties(element);
+    }
+
+
+    public void SetConnectionFlow(ConnectionFlow flow)
+    {
+      var controller = new CanvasController(canvas);
+
+      controller.SetConnectionFlow(flow);
+    }
+
+    public void ToggleConnectionFlow(ConnectionFlow flow)
+    {
+      var f = flow == ConnectionFlow.OneWay ? ConnectionFlow.TwoWay : ConnectionFlow.OneWay;
+      var controller = new CanvasController(canvas);
+
+      controller.SetConnectionFlow(f);
+    }
+
+    public void SetConnectionStyle(ConnectionStyle style)
+    {
+      var controller = new CanvasController(canvas);
+
+      controller.SetConnectionStyle(style);
+    }
+
+    public void ToggleConnectionStyle(ConnectionStyle style)
+    {
+      var f = style == ConnectionStyle.Dashed ? ConnectionStyle.Solid : ConnectionStyle.Dashed;
+      var controller = new CanvasController(canvas);
+
+      controller.SetConnectionStyle(f);
+    }
+
+    public void SetConnectionLabel(ConnectionLabel label)
+    {
+      var controller = new CanvasController(canvas);
+
+      controller.SetConnectionLabel(label);
+    }
+
+    public void MakeVisible(Element element)
+    {
+      var controller = new CanvasController(canvas);
+      controller.EnsureVisible(element);
+    }
+  }
+}

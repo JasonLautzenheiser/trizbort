a57f601dfb658eca4852d05558bf6f65a21a8fbd
Jason Lautzenheiser <lautzenheiser.jason@gmail.com>
2018-03-03 08:56:27 -0500

    refactor (map) - refactor loading of map, extracting out load classes opening door for libraries to be written for other map formats to be loaded

---
+++ Domain/Application/MapFileEngine.cs
@@ -0,0 +1,18 @@
+﻿namespace Trizbort.Domain.Application {
+  public abstract class MapFileEngine {
+    private string fileName;
+    protected MapFileEngine() { }
+
+    protected MapFileEngine(string fileName) {
+      this.fileName = fileName;
+    }
+
+    public virtual bool Load() {
+      return Load(fileName);
+    }
+
+    public virtual bool Load(string fileName) {
+      return true;
+    }
+  }
+}
--- Domain/Application/Project.cs
+++ Domain/Application/Project.cs
@@ -171,85 +171,11 @@ namespace Trizbort.Domain.Application
     {
       FileWatcher.ReloadMap -= ReloadMap;
 
-      try
-      {
-        if (new FileInfo(FileName).Length == 0)
-        {
-          // this is an empty file, probably thanks to our Explorer New->Trizbort Map menu option.
-          Settings.Reset();
-          return true;
-        }
-
-        var doc = new XmlDocument();
-        doc.Load(FileName);
-        var root = new XmlElementReader(doc.DocumentElement);
-
-        if (!root.HasName("trizbort"))
-          throw new InvalidDataException(string.Format("Not a {0} map file.", System.Windows.Forms.Application.ProductName));
-
-        //reset checks: we may make this into a function if we ever wish to verify a Trizbort file first.
-        Settings.StartRoomLoaded = false;
-        Settings.EndRoomLoaded = false;
-
-        // file version
-        var versionNumber = root.Attribute("version").Text;
-        setVersion(versionNumber);
-        checkDocVersion();
-
-        // load info
-        Title = root["info"]["title"].Text;
-        Author = root["info"]["author"].Text;
-        Description = root["info"]["description"].Text;
-        History = root["info"]["history"].Text;
-
-        // load all elements
-        var map = root["map"];
-        var mapConnectionToLoadState = new Dictionary<Connection, object>();
-        foreach (var element in map.Children)
-          if (element.HasName("room"))
-          {
-            // Changed the constructor used for elements when loading a file for a significant speed increase
-            var room = new Room(this, Elements.Count + 1);
-            room.ID = element.Attribute("id").ToInt(room.ID);
-            room.Load(element);
-            Elements.Add(room);
-          }
-          else if (element.HasName("line"))
-          {
-            // Changed the constructor used for elements when loading a file for a significant speed increase
-            var connection = new Connection(this, Elements.Count + 1);
-            connection.ID = element.Attribute("id").ToInt(connection.ID);
-            var loadState = connection.BeginLoad(element);
-            if (loadState != null)
-              mapConnectionToLoadState.Add(connection, loadState);
-            Elements.Add(connection);
-          }
-
-        // connect them together
-        foreach (var pair in mapConnectionToLoadState)
-        {
-          var connection = pair.Key;
-          var state = pair.Value;
-          connection.EndLoad(state);
-        }
-
-        // load settings last, since their load can't be undone
-        Settings.Reset();
-        Settings.Load(root["settings"]);
-
-        // setup filewatcher.
-        FileWatcher.ReloadMap += ReloadMap;
-        FileWatcher.InitializeWatcher(FileName);
-        return true;
-      }
-      catch (Exception ex)
-      {
-        MessageBox.Show(Program.MainForm, $"There was a problem loading the map:{Environment.NewLine}{Environment.NewLine}{ex.Message}", System.Windows.Forms.Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
-        return false;
-      }
+      var loader = new MapLoader(this);
+      return loader.LoadMap(FileName);
     }
 
-    private void checkDocVersion()
+    public void CheckDocVersion()
     {
       var appVers = Version.Parse(System.Windows.Forms.Application.ProductVersion);
       var infoList = $"Executable Version = {System.Windows.Forms.Application.ProductVersion}{Environment.NewLine}Document Version = {Version}{Environment.NewLine}{Environment.NewLine}";
@@ -283,7 +209,7 @@ namespace Trizbort.Domain.Application
       }
     }
 
-    private void setVersion(string versionNumber)
+    public void SetVersion(string versionNumber)
     {
       try { Version = Version.Parse(versionNumber); }
       catch (Exception)
@@ -396,6 +322,11 @@ namespace Trizbort.Domain.Application
     public void Dispose()
     {
       FileWatcher.ReloadMap -= ReloadMap;
+    }
+
+    public void InitFileWWatcher(string fileName) {
+      FileWatcher.ReloadMap += ReloadMap;
+      FileWatcher.InitializeWatcher(fileName);
     }
   }
 }

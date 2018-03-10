ebfc7a485b53b33437630c36ad2dacb982f70adc
Jason Lautzenheiser <lautzenheiser.jason@gmail.com>
2018-03-10 10:49:40 -0500

    fix: fix issue where Paste menu item was not enabled properly (#417)

---
+++ Util/ClipboardHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Trizbort.Util {
+  public static class ClipboardHelper {
+    public static bool HasSomethingToPaste() {
+      if (string.IsNullOrEmpty(Clipboard.GetText()))
+        return false;
+
+      return true;
+    }
+  }
+}

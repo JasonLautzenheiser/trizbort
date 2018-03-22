02a5634a44bd7b140f002cec1256be1cc348395b
Jason Lautzenheiser <lautzenheiser.jason@gmail.com>
2018-03-21 21:09:45 -0400

    change: change the room properties by removing warning about multiple end rooms
    Also fixed the context menu where if you had multiple rooms selected the end
    room menu was disabled.  There was no reason for that.

--- UI/RoomPropertiesDialog.cs
+++ UI/RoomPropertiesDialog.cs
@@ -901,21 +901,6 @@ namespace Trizbort.UI
       e.TooltipInfo.BodyText = e.TooltipInfo.BodyText.Replace("{gridsize}", Settings.GridSize.ToString(CultureInfo.CurrentCulture));
     }
 
-    private void chkEndRoom_CheckedChanged(object sender, EventArgs e)
-    {
-      if (chkEndRoom.Checked)
-      {
-        var list = Project.Current.Elements.OfType<Room>().Where(p => p.IsEndRoom && p.ID != roomID).ToList();
-
-        if (list.Count <= 0) return;
-
-        if (MessageBox.Show($"The room '{list.First().Name}' is set as the ending room.  Do you want to change it to this room?", "Change Ending Room", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-          Project.Current.Elements.OfType<Room>().ToList().ForEach(p => p.IsEndRoom = false);
-        else
-          chkEndRoom.Checked = false;
-      }
-    }
-
     private void m_subTitleTextTextBox_ButtonCustomClick(object sender, EventArgs e)
     {
       RoomSubtitleColor = Color.Transparent;

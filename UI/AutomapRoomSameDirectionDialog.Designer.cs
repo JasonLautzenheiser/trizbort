5479a0f364c4175b49e6ee86f29296b964b19de0
Jason Lautzenheiser <lautzenheiser.jason@gmail.com>
2018-05-20 08:26:54 -0400

    remove dotnetbar from automapsamedirection dialog

--- UI/AutomapRoomSameDirectionDialog.Designer.cs
+++ UI/AutomapRoomSameDirectionDialog.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Trizbort.UI
+﻿using System.Drawing;
+
+namespace Trizbort.UI
 {
   partial class AutomapRoomSameDirectionDialog
   {
@@ -28,31 +30,27 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.btnRoom1 = new DevComponents.DotNetBar.ButtonX();
-      this.btnRoom2 = new DevComponents.DotNetBar.ButtonX();
-      this.lblMessage = new DevComponents.DotNetBar.LabelX();
-      this.btnKeepBoth = new DevComponents.DotNetBar.ButtonX();
+      this.btnRoom1 = new System.Windows.Forms.Button();
+      this.btnRoom2 = new System.Windows.Forms.Button();
+      this.lblMessage = new System.Windows.Forms.Label();
+      this.btnKeepBoth = new System.Windows.Forms.Button();
       this.SuspendLayout();
       // 
       // btnRoom1
       // 
       this.btnRoom1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-      this.btnRoom1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
       this.btnRoom1.Location = new System.Drawing.Point(261, 13);
       this.btnRoom1.Name = "btnRoom1";
       this.btnRoom1.Size = new System.Drawing.Size(167, 23);
-      this.btnRoom1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
       this.btnRoom1.TabIndex = 0;
       this.btnRoom1.Click += new System.EventHandler(this.btnRoom1_Click);
       // 
       // btnRoom2
       // 
       this.btnRoom2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-      this.btnRoom2.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
       this.btnRoom2.Location = new System.Drawing.Point(261, 42);
       this.btnRoom2.Name = "btnRoom2";
       this.btnRoom2.Size = new System.Drawing.Size(167, 23);
-      this.btnRoom2.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
       this.btnRoom2.TabIndex = 1;
       this.btnRoom2.Click += new System.EventHandler(this.btnRoom2_Click);
       // 
@@ -61,23 +59,18 @@
       // 
       // 
       // 
-      this.lblMessage.BackgroundStyle.Class = "";
-      this.lblMessage.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
       this.lblMessage.Location = new System.Drawing.Point(13, 13);
       this.lblMessage.Name = "lblMessage";
       this.lblMessage.Size = new System.Drawing.Size(242, 81);
       this.lblMessage.TabIndex = 2;
-      this.lblMessage.TextLineAlignment = System.Drawing.StringAlignment.Near;
-      this.lblMessage.WordWrap = true;
+      this.lblMessage.TextAlign = ContentAlignment.MiddleLeft;
       // 
       // btnKeepBoth
       // 
       this.btnKeepBoth.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-      this.btnKeepBoth.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
       this.btnKeepBoth.Location = new System.Drawing.Point(261, 71);
       this.btnKeepBoth.Name = "btnKeepBoth";
       this.btnKeepBoth.Size = new System.Drawing.Size(167, 23);
-      this.btnKeepBoth.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
       this.btnKeepBoth.TabIndex = 3;
       this.btnKeepBoth.Text = "Keep Both";
       this.btnKeepBoth.Click += new System.EventHandler(this.btnKeepBoth_Click);
@@ -103,9 +96,9 @@
 
     #endregion
 
-    private DevComponents.DotNetBar.ButtonX btnRoom1;
-    private DevComponents.DotNetBar.ButtonX btnRoom2;
-    private DevComponents.DotNetBar.LabelX lblMessage;
-    private DevComponents.DotNetBar.ButtonX btnKeepBoth;
+    private System.Windows.Forms.Button btnRoom1;
+    private System.Windows.Forms.Button btnRoom2;
+    private System.Windows.Forms.Label lblMessage;
+    private System.Windows.Forms.Button btnKeepBoth;
   }
 }

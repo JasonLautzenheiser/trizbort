ba563a21cd76d390d7607554c3fb7012591b04ac
Jason Lautzenheiser <lautzenheiser.jason@gmail.com>
2018-06-03 15:17:40 -0400

    refactored toolbar to make more extensible cleaned up zoom statusbar widget added caps lock and num lock statusbar widget

---
+++ Domain/StatusBar/CapsLockStatusWidget.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using Trizbort.Util;
+
+namespace Trizbort.Domain.StatusBar {
+  public class CapsLockStatusWidget : IStatusWidget {
+    public StatusItems Id => StatusItems.tsb_CapsLock;
+    public string Name => "CAPS Lock";
+    public string MenuName => "Caps Lock State";
+    public string HelpText => "Caps Lock State, Left click to change.";
+
+    public Color DisplayColor => Control.IsKeyLocked(Keys.CapsLock) ? Color.Black : Color.DarkGray;
+
+    public string DisplayText() {
+      return "CAPS";
+    }
+
+    public void ClickHandler() {
+      KeyboardHelper.SetCapsLockKey(!Control.IsKeyLocked(Keys.CapsLock));
+    }
+
+
+
+
+  }
+}
